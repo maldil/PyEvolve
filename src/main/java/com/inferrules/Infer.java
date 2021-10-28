e07bf2b714db8532f5a3587fd366d55b7fab7990
ChangeType <javatypechange@gmail.com>
2021-10-27 18:08:16 -0700

    distributable zip, command line args

--- src/main/java/com/inferrules/Infer.java
+++ src/main/java/com/inferrules/Infer.java
@@ -1,52 +1,17 @@
 package com.inferrules;
 
 import com.inferrules.core.RewriteRule;
-import com.inferrules.core.languageAdapters.LanguageSpecificInfo;
-import org.apache.commons.cli.*;
+import com.inferrules.utils.Utilities;
 
-import java.util.Map;
+import static com.inferrules.core.languageAdapters.LanguageSpecificInfo.*;
 
 public class Infer {
 
     public static void main(String[] args) {
-        var m = parseCommandLineArgs(args);
-        InputFormat input = new InputFormat(m.get("Before"), m.get("After"), m.get("Language"));
-
-        RewriteRule rw = new RewriteRule(input.getBefore(),
-                input.getAfter(),
-                LanguageSpecificInfo.Language.valueOf(input.getLanguage()));
+        var input = Utilities.parseCommandLineArgs(args);
+        RewriteRule rw = new RewriteRule(input.get("Before"), input.get("After"), Language.valueOf(input.get("Language")));
         System.out.println(rw.getMatch().getTemplate());
         System.out.println("---------------");
         System.out.println(rw.getReplace().getTemplate());
-    }
-
-
-    public static Map<String, String> parseCommandLineArgs(String[] args){
-        Options options = new Options();
-
-        Option input = new Option("b", "before", true, "Before Code Snippet");
-        input.setRequired(true);
-        options.addOption(input);
-
-        Option output = new Option("a", "after", true, "After Code Snippet");
-        output.setRequired(true);
-        options.addOption(output);
-
-        Option lang = new Option("l", "language", true, "Language");
-        lang.setRequired(true);
-        options.addOption(lang);
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = null;//not a good practice, it serves it purpose
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("utility-name", options);
-            System.exit(1);
-        }
-        return Map.of("Before", cmd.getOptionValue("before").replace("\\n","\n"), "After",cmd.getOptionValue("after").replace("\\n","\n"),
-                "Language",cmd.getOptionValue("language"));
     }
 }
--- src/main/java/com/inferrules/utils/Utilities.java
+++ src/main/java/com/inferrules/utils/Utilities.java
@@ -15,6 +15,7 @@ import static java.util.stream.Collectors.toMap;
 
 import io.vavr.control.Try;
 import org.antlr.v4.runtime.misc.Interval;
+import org.apache.commons.cli.*;
 
 public class Utilities {
 
@@ -54,4 +55,32 @@ public class Utilities {
     }
 
 
+    public static Map<String, String> parseCommandLineArgs(String[] args){
+        Options options = new Options();
+
+        Option input = new Option("b", "before", true, "Before Code Snippet");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("a", "after", true, "After Code Snippet");
+        output.setRequired(true);
+        options.addOption(output);
+
+        Option lang = new Option("l", "language", true, "Language");
+        lang.setRequired(true);
+        options.addOption(lang);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;//not a good practice, it serves it purpose
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
+        return Map.of("Before", cmd.getOptionValue("before").replace("\\n","\n"), "After",cmd.getOptionValue("after").replace("\\n","\n"),
+                "Language",cmd.getOptionValue("language"));
+    }
 }
